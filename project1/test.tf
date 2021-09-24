afe8cd1c25f850aa135f5d5db74a5b343e649678
Charles Dunda <cdunda@2u.com>
2021-09-24 13:35:27 -0400

    remove ebs_block_device

--- project1/test.tf
+++ project1/test.tf
@@ -5,12 +5,6 @@ resource "aws_ami" "example" {
   name                = "${var.cp_tf_var}-${terraform.workspace}"
   virtualization_type = "hvm"
   root_device_name    = "/dev/xvda"
-
-  ebs_block_device {
-    device_name = "/dev/xvda"
-    snapshot_id = "snap-xxxxxxxx"
-    volume_size = 8
-  }
 }
 
 variable "cp_tf_var" {}
