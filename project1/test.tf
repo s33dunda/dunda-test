376785a49e834a472a813aeb82c6397f1d1ef065
Charles Dunda <cdunda@2u.com>
2021-09-27 17:08:43 -0400

    Add snap

--- project1/test.tf
+++ project1/test.tf
@@ -5,6 +5,12 @@ resource "aws_ami" "example" {
   name                = "${var.cp_tf_var}-${terraform.workspace}"
   virtualization_type = "hvm"
   root_device_name    = "/dev/xvda"
+
+  ebs_block_device {
+    device_name = "/dev/xvda"
+    snapshot_id = "snap-0b0409130922ca238"
+    volume_size = 8
+  }
 }
 
 variable "cp_tf_var" {}
